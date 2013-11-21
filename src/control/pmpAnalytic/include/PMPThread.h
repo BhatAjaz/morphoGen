5c069062cedba6a5188bb36e82474632a963a39f
reafrancesco <reafrancesco@4ddd3772-fbed-4136-9ecf-17122a9aa07d>
2013-11-21 11:04:10 +0000

    fine tuned the behaviour of the system

--- src/control/pmpAnalytic/include/PMPThread.h
+++ src/control/pmpAnalytic/include/PMPThread.h
@@ -49,6 +49,7 @@
 #define COMMAND_VOCAB_ORIG   VOCAB4('O','R','I','G')
 #define COMMAND_VOCAB_WRIO   VOCAB4('W','R','I','O')
 #define COMMAND_VOCAB_FAILED VOCAB4('F','A','I','L')
+#define COMMAND_VOCAB_INIT   VOCAB4('I','N','I','T')
 #define COMMAND_VOCAB_REA    VOCAB3('R','E','A')
 #define COMMAND_VOCAB_OK     VOCAB2('O','K')
 
